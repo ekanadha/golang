d9b4f1f530ac7d756712f0d6e6fe2d7bf0210deb
ekanadha <96290319+ekanadha@users.noreply.github.com>
2022-07-11 20:44:40 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,23 +1,21 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+        "fmt"
+        "log"
+        "net/http"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, " jai krishna !!!")
+        fmt.Fprintf(w, " jai krishna !!!")
 
 }
 
 func handleRequest() {
-	http.HandleFunc("/", homePage)
-	log.Fatal(http.ListenAndServe("0.0.0.0:8081", nil))
+        http.HandleFunc("/", homePage)
+        log.Fatal(http.ListenAndServe("0.0.0.0:8081", nil))
 }
 
 func main() {
-	handleRequest()
+        handleRequest()
 }
-
-
